58556c885fac29af985ce0a1d6419228384fe2fe
Libikk <libikpl@gmail.com>
2022-02-22 19:14:49 +0000

    chore: init stuff

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+import { RadioGroup } from '@headlessui/react'
+import './index.css';
+
+const classNames = (...classes)  => classes.filter(Boolean).join(' ')
+export default function App() {
+
+  return (
+    <div className="bg-red-500">
+      <div>
+        test
+      </div>
+    </div>
+  )
+}
