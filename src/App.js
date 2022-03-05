389444797166419205f8448ce8956fbbada31ed4
Libikk <libikpl@gmail.com>
2022-03-05 09:11:50 +0000

    feat: first init query from FE to BE

--- src/App.js
+++ src/App.js
@@ -1,15 +1,41 @@
-import { useState } from 'react';
-import { RadioGroup } from '@headlessui/react'
 import './index.css';
-
+import { gql, useQuery } from '@apollo/client';
+const SEARCH_FOOD_QUERY = gql`
+query {
+  searchFoods(query: "pepsi" sortOrder:{
+    field: id
+    direction: ASC
+  }) {
+    totalPages
+    foods {
+      id
+      dataType
+      publishedDate
+      description
+      marketCountry
+      brandName
+      foodCategory
+    }
+    currentPage
+  }
+}
+`;
 const classNames = (...classes)  => classes.filter(Boolean).join(' ')
 export default function App() {
 
-  return (
-    <div className="bg-red-500">
-      <div>
-        test
-      </div>
-    </div>
-  )
+  const { loading, error, data } = useQuery(SEARCH_FOOD_QUERY);
+  console.log('🚀 ~ file: App.js ~ line 29 ~ App ~ error', error);
+  console.log('🚀 ~ file: App.js ~ line 29 ~ App ~ data', data);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+
+  // return data.rates.map(({ currency, rate }) => (
+  //   <div key={currency}>
+  //     <p>
+  //       {currency}: {rate}
+  //     </p>
+  //   </div>
+  // ));
+  return <div>{data && JSON.stringify(data)}</div>
 }
